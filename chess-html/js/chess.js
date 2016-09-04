caa373087db385fac665eef1742e128f53acde07
Sahil Jain <sahil@sahiljain.ca>
2016-09-04 15:42:26 -0400

    add frontend code

---
+++ chess-html/js/chess.js
@@ -0,0 +1,51 @@
+var board,
+  game = new Chess();
+
+// do not pick up pieces if the game is over
+// only pick up pieces for White
+var onDragStart = function(source, piece, position, orientation) {
+  if (game.in_checkmate() === true || game.in_draw() === true ||
+    piece.search(/^b/) !== -1) {
+    return false;
+  }
+};
+
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  });
+
+  // illegal move
+  if (move === null) return 'snapback';
+  // make random legal move for black
+
+  //window.setTimeout(makeRandomMove, 250);
+  console.log('sending board to server: ' + game.fen());
+  $.get("http://localhost:8080/move?fen=" + game.fen(), function(data) {
+    console.log("response received");
+    var success = game.load(data);
+    if (!success) {
+      alert("FAIL");
+    }
+    board.position(data);
+  });
+};
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+var onSnapEnd = function() {
+  board.position(game.fen());
+};
+
+var cfg = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+board = ChessBoard('board', cfg);
+
